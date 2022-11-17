6ccf6e0958817294bccacd5ac81b3c561e6f0e10
a.breezy <ambrose.wilkins@gmail.com>
2022-11-17 09:24:32 -0500

    add fk_manager constrain to reference employees to manager id

--- db/schema.sql
+++ db/schema.sql
@@ -12,15 +12,15 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     dept_id INTEGER,
-    CONSTRAINT dept_id FOREIGN KEY (dept_id) REFERENCES department(id) ON DELETE CASCADE
+    CONSTRAINT fk_dept FOREIGN KEY (dept_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INTEGER AUTOR_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) 
 )
